4d06506de772faa4ae4a9d73132b2b8de706fcbb
Satyamsinghweb <satyamsinghxspam@gmail.com>
2026-04-05 19:39:47 +0530

    mastering js

---
+++ video55/script.js
@@ -0,0 +1,29 @@
+console.log("hii this is 2nd tutorial")
+let a=5;
+let b=6;
+var c="satyam"
+console.log(a+b+9);
+console.log(typeof a,typeof b, typeof c)
+/*var 66a ="shubham" not allowed bcz it cant start with a no.
+const a1=6;(constant variable)
+a1=a1+1;(not allowed bcz it is constant)*/
+{
+    let a =66
+    console.log(a)
+}
+console.log(a)
+let x = "kriss";
+let y= 56;
+let z= 98.5;
+const p=true;
+let q=undefined;
+let r=null;
+console.log(x,y,z,p,q,r)/*(null object hota hai )*/
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+let o={
+    "name":"satyam",
+    "job code":5000
+}
+console.log(o)
+o.salary=500000;
+console.log(o)

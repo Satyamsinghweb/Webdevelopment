fa4debc92d9ed4482d4d98b605624c5d736bc90c
Satyamsinghweb <satyamsinghxspam@gmail.com>
2026-04-11 02:43:31 +0530

    mastering js

---
+++ video56/script.js
@@ -0,0 +1,17 @@
+console.log("hello from satyam")
+let age=51;
+let grace=2;
+age+=grace/*(increment operator)*/
+console.log(age)
+console.log(age %  grace)
+if(age>18){
+    console.log("you can drive ")
+}
+else{
+    console.log("you can't drive")
+}
+/*=== equal value and  type*/
+let a=5;
+let b=17;
+c=(a>b)?(a-b):(b-a)/*ternary opr*/
+console.log(c)

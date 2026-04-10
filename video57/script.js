be4d828d0c801305c10ca660f5ad0a059de7151b
Satyamsinghweb <satyamsinghxspam@gmail.com>
2026-04-11 02:44:00 +0530

    mastering js

---
+++ video57/script.js
@@ -0,0 +1,26 @@
+console.log("i am a tutorial on for loops")
+let a=1;
+for (let index = 0; index < 99; index++) {
+    console.log(a+index)  
+}      
+let obj = {
+    name:"satyam",
+    role:"developer",
+    company:"satyam ai"
+}      
+for (const key in obj) { 
+        const element = obj[key];
+        console.log(element)
+}    
+for (const element of "satyam") {
+    console.log(element)
+}
+i=6;
+while(i<7){
+    console.log(i)
+    i++;
+}
+
+      
+    
+          

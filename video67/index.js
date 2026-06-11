24a4cba542ac040ba9a1530a63965555e1af1979
Satyamsinghweb <satyamsinghxspam@gmail.com>
2026-06-12 02:46:12 +0530

    Mastering js

---
+++ video67/index.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+// let cont=document.body.childNodes[1]
+// undefined
+// cont.lastElementChild
+// <div class=​"box">​box5​</div>​
+// cont.lastChild
+// #text
+// document.body.children
+// HTMLCollection(2) [div.container, script]
+// document.body.firstElementChild.children
+// HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​
